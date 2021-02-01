577031b70b09ff911088491485f986de17cdc3fe
Pulkit Chaudhary <pulkithb@gmail.com>
2021-02-01 21:13:43 +0530

    sign in button update

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -29,7 +29,7 @@ const Navigation = ({onRouteChange,route}) =>{
                 <span className='bar1 '>
                 <a className='but1 shadow-5 link pointer dim grow dib br3 ma2 tc' href='https://pulkithb.github.io'>My Website</a>
                 <a  onClick={ () => onRouteChange('register')} className='but1 shadow-5 link pointer dim grow dib br3 ma2 tc'>Register</a>
-                <a onClick={ () => onRouteChange('signin')} className='but shadow-5 link pointer dim grow dib br3 ma2 tc' >Sign In</a>
+                <a onClick={ () => onRouteChange('signin')} className='but1 shadow-5 link pointer dim grow dib br3 ma2 tc' >Sign In</a>
                     
                     
                 </span>
