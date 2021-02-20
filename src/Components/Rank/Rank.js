9009b94224f613bc8ec0896c60fd020584a4a0b2
Pulkit Chaudhary <pulkithb@gmail.com>
2021-02-20 12:24:13 +0530

    Upper Case edit

--- src/Components/Rank/Rank.js
+++ src/Components/Rank/Rank.js
@@ -4,7 +4,7 @@ const Rank = ({name,entries}) => {
     return(
         <div className='tc washed-yellow pa5'>
             <div className='greeting'>
-                {'Welcome '}<b>{name}</b>{', your current entry number is '}
+                {'Welcome '}<b>{name}</b>{', your current Entry Number is '}
                 
             </div>
             <div className='score'>
